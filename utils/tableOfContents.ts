f2cde6c48fddbce64eb878c7f96b5ba427de9934
domscript <vladimirndsm@gmail.com>
2022-11-22 18:36:11 +0700

    add tableOfContent

---
+++ utils/tableOfContents.ts
@@ -0,0 +1,59 @@
+import { MatchCategory } from "../types/MatchCategory";
+
+const shortExpl = {
+  [MatchCategory.Fonts]:
+    "Websites that offer free fonts as well as font-based tools",
+  [MatchCategory.Colors]:
+    "Websites and resources that help with choices related to colors",
+  [MatchCategory.Icons]: "Resources for Icons including png, svg and more",
+  [MatchCategory.Logos]: "Resources for Logos",
+  [MatchCategory.Favicons]: "Resources for Favicons",
+  [MatchCategory.IconFonts]: "Resources for Icon Fonts",
+  [MatchCategory.StockPhotos]:
+    "Websites that offer free stock photos of all kinds for your websites and apps",
+  [MatchCategory.StockVideos]:
+    "Websites that offer free stock videos of all kinds for your websites and apps",
+  [MatchCategory.StockMusicSoundEffects]:
+    "Websites that offer free stock music and/or sound effects",
+  [MatchCategory.VectorsClipArt]:
+    "Free vectors, clipart, illustrations, patterns and more",
+  [MatchCategory.ProductImageMockups]:
+    "Create mockups of devices and other products",
+  [MatchCategory.HTMLCSSTemplates]:
+    "Websites that offer free beautiful website templates and themes of all types",
+  [MatchCategory.CSSFrameworks]:
+    "CSS/UI frameworks to help build great looking websites and applications",
+  [MatchCategory.CSSMethodologies]:
+    "CSS methodologies to help write modular, reusable and scalable code",
+  [MatchCategory.CSSAnimations]:
+    "CSS animations to build awesome animations for websites and applications",
+  [MatchCategory.JavaScriptAnimationLibraries]:
+    "JavaScript animations libraries to build awesome animations for websites and applications",
+  [MatchCategory.JavaScriptChartLibraries]:
+    "Libraries that help developers visualize data into charts",
+  [MatchCategory.UIComponentsKits]:
+    "Not quite 'frameworks', but tools for creating user interfaces with components or UI kits",
+  [MatchCategory.ReactUILibraries]:
+    "UI and component libraries for the React JavaScript framework",
+  [MatchCategory.VueUILibraries]:
+    "UI and component libraries for the Vue JavaScript framework",
+  [MatchCategory.AngularUILibraries]:
+    "UI and component libraries for the Angular JavaScript framework",
+  [MatchCategory.SvelteUILibraries]:
+    "UI and component libraries for the Svelte JavaScript compiler",
+  [MatchCategory.ReactNativeUILibraries]:
+    "UI and component libraries for the React Native Framework",
+  [MatchCategory.DesignSystemsStyleGuides]:
+    "Design systems, style guides, toolkits, docs. Some of these are design guides for top companies/websites",
+  [MatchCategory.OnlineDesignTools]:
+    "All kinds of online tools for design, from photo editors to wireframing, and more",
+  [MatchCategory.DownloadableDesignSoftware]:
+    "Free software for UI, photo, 3d modeling, etc. Alternatives to paid software like Photoshop",
+  [MatchCategory.DesignInspiration]:
+    "Here are some websites to get inspiration for design and UI",
+  [MatchCategory.ImageCompression]:
+    "Websites that allow you to compress large images",
+  [MatchCategory.ChromeExtensions]:
+    "Useful Chrome extensions for Designers and Web-Developers.",
+  [MatchCategory.Others]: "Uncategorized Stuff",
+};
